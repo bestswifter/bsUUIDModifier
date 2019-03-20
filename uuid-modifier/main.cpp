8637827a567b1e662807a8139751b83295b934ce
zhangxingyu.bs <zhangxingyu.bs@bytedance.com>
2019-03-20 14:41:35 +0800

    Initial Commit

---
+++ uuid-modifier/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  uuid-modifier
+//
+//  Created by 张星宇 on 2019/3/20.
+//  Copyright © 2019 张星宇. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
